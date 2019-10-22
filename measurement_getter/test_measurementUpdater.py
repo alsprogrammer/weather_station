f746f32b51f612e30f52a833d2891c4baa74fea5
Aleksandr Saiapin <alstutor@gmail.com>
2019-10-22 15:50:56 +0700

    Add measurement updater test.

---
+++ measurement_getter/test_measurementUpdater.py
@@ -0,0 +1,36 @@
+from typing import Union
+from unittest import TestCase
+
+from measurement_getter.getters.measurement_getter_abc import MeasurementGetter
+from measurement_getter.measurement_updater import MeasurementUpdater
+from measurement_getter.senders.sender_abc import MeasurementSender
+from utils.time_utils import get_current_timestamp
+
+MEASUREMENT_VALUE = 735
+
+
+class MockMeasurementGetter(MeasurementGetter):
+    def __call__(self):
+        return MEASUREMENT_VALUE
+
+
+class MockMeasurementSender(MeasurementSender):
+    def __call__(self, value: Union[int, float], timestamp: int):
+        self.value = value
+        self.timestamp = timestamp
+
+
+class TestMeasurementUpdater(TestCase):
+    def setUp(self) -> None:
+        self.getter = MockMeasurementGetter()
+        self.sender = MockMeasurementSender()
+
+        self.updater = MeasurementUpdater(self.getter, self.sender)
+
+    def test_get_and_send_measurement(self):
+        timestamp_before = get_current_timestamp()
+        self.updater.get_and_send_measurement()
+        timestamp_after = get_current_timestamp()
+        self.assertEqual(self.sender.value, MEASUREMENT_VALUE)
+        self.assertGreaterEqual(self.sender.timestamp, timestamp_before)
+        self.assertLessEqual(self.sender.timestamp, timestamp_after)
